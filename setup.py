243f18cc2f2e6b3f651bf47128435cd0caaf9a5f
chuanzong.lcz <chuanzong.lcz@alibaba-inc.com>
2016-08-14 00:02:54 +0800

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='ColorPrinter',
+    version='0.1.0',
+    license="MIT",
+    description='Print Message to Terminal with Colored Header',
+    author='ChuanZong Liu',
+    author_email='liuchzong@qq.com',
+    url='https://github.com/legend80s/ColorPrinter',
+    download_url='https://github.com/legend80s/ColorPrinter/tarball/0.1.0',
+    keywords=['color', 'print', 'colorprint', 'color print'],
+    classifiers=[],
+)
